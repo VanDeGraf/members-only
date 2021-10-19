b4f61fd161e7d1bd53fc5ab13c10c31677ec9eb0
VanDeGraf <vandegraf@bk.ru>
2021-10-19 22:13:08 +0400

    Refactoring remove RegistrationsController
    Move it's logic to ApplicationController by follow devise instructions

--- app/controllers/registrations_controller.rb
+++
@@ -1,6 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-
-  protected def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-end
